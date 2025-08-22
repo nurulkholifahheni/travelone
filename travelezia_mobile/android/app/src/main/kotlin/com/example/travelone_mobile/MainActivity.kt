0715fa997cf36ff22ac56b40bd54fdb39d1b9cfb
Abdullah Azzam <dfeqhasavrymchaplctionnphysics@gmail.com>
2025-08-22 08:32:50 +0700

    refactoring

---
+++ travelezia_mobile/android/app/src/main/kotlin/com/example/travelone_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.travelezia_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- travelone_mobile/android/app/src/main/kotlin/com/example/travelone_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.travelone_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
